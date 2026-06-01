91f5865be7cd778dd50b9632540acca5fa423155
minhtc <minhtc.git@homielab.com>
2026-06-01 10:36:38 +0700

    feat: add Docker support

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: process.env.BUILD_STANDALONE === "1" ? "standalone" : undefined,
+};
 
 export default nextConfig;
